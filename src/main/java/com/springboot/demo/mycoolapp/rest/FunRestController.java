bbc6a5d72fb8a7113bed797c13839e798ee4e59c
DongDuy <dongduy.dev@gmail.com>
2026-03-12 00:29:39 +0700

    simple restcontroller

--- src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,14 +1,31 @@
 package com.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fun")
 public class FunRestController {
     //expose "/" that return "Hello World"
 
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @GetMapping("/porsche")
+    public String sayPorsche() {
+        return "Porsche 911";
+    }
+}
+
+
+@RestController
+@RequestMapping("/funny")
+class FunnyRestController {
+    @GetMapping("/stoic")
+    public String sayStoic() {
+        return "Stoic";
     }
 }
